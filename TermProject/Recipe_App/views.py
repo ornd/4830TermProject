80909798ec50f5a165f30677265267efee6f0f88
ornd <48333719+ornd@users.noreply.github.com>
2019-10-02 22:31:01 -0500

    Display Recipe

--- TermProject/Recipe_App/views.py
+++ TermProject/Recipe_App/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+#from django.http import HttpResponse
+#from django.template import loader
+#from django.http import Http404
 
 # Create your views here.
+from .models import Recipe_List
 def index(request):
-    return HttpResponse("Hello, world. This is the recipe index")
+    all_recipes = Recipe_List.objects.order_by('id')[:]
+#    template = loader.get_template('Recipe_App/index.html')
+    context = {
+        'all_recipes': all_recipes,
+    }
+#    return HttpResponse(template.render(context, request))
+    return render(request, 'Recipe_App/index.html', context)
+
+#    output = '\n '.join([str(i + 1) + '.) ' + q.name_text for i, q in enumerate(all_recipes)])
+#    return HttpResponse(output)
+
+def detail(request, Recipe_List_id):
+    recipe = get_object_or_404(Recipe_List, pk = Recipe_List_id)
+    print(recipe)
+#    try:
+#        recipe = Recipe_List.objects.get(pk = Recipe_List_id)
+#    except Recipe_List.DoesNotExist:
+#        raise Http404("Recipe does not exist")
+    return render(request, 'Recipe_App/detail.html', {'recipe': recipe})
+#    return HttpResponse('You\'re looking at recipe number ' + str(Recipe_List_id))
